618c9023ceca79795c481f28b4ff95d7271016c9
KoningR <r.m.koning@student.tudelft.nl>
2021-12-01 16:30:16 +0100

    Add support for reading Eurotoken commands from a file.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,56 +1,78 @@
+import mu.KLogger
 import mu.KotlinLogging
+import java.io.File
+import java.io.FileNotFoundException
 
-fun main() {
+fun executeLine(inputString: String, app: Application, logger: KLogger): Boolean {
+//    val input = readLine()!!.toLowerCase().split(" ")
+    val input = inputString.toLowerCase().split(" ")
+
+    when (input[0]) {
+        "info" -> {
+            app.printInfo()
+        }
+        "get_balance" -> {
+            logger.info("Your balance is ${app.getBalance()} Eurotoken.")
+        }
+        "create_coin" -> {
+            if (input.size == 2) {
+                app.createCoin(input[1].toLong())
+            } else {
+                app.createCoin()
+            }
+
+            logger.info("Generated Eurotoken. Your balance is now ${app.getBalance()} Eurotoken.")
+        }
+        "send_coin" -> {
+            if (input.size == 3) {
+                app.sendCoin(input[1], input[2].toLong())
+            } else {
+                app.sendCoin()
+            }
+
+            logger.info("Your balance is now ${app.getBalance()} Eurotoken.")
+        }
+        "transaction_history" -> {
+            if (input.size == 2) {
+                app.transactionHistory(input[1])
+            }
+        }
+        "stop" -> {
+            app.stop()
+
+            logger.info("Shutting down...")
+            return false
+        }
+        else -> {
+            logger.info("Unknown command")
+        }
+    }
+
+    return true
+}
+
+fun main(args: Array<String>) {
     val logger = KotlinLogging.logger {}
     logger.info("Starting Eurotoken client...")
 
     val app = Application()
 
-    var running = true
-    while (running) {
+    val reader = try {
+        File(args[0]).inputStream().bufferedReader()
+    } catch (e: FileNotFoundException) {
+        logger.info("Could not find script file.")
+        return
+    }
 
-        val input = readLine()!!.toLowerCase().split(" ")
+    reader.forEachLine {
+        executeLine(it, app, logger)
+    }
 
-        when (input[0]) {
-            "info" -> {
-                app.printInfo()
-            }
-            "get_balance" -> {
-                logger.info("Your balance is ${app.getBalance()} Eurotoken.")
-            }
-            "create_coin" -> {
-                if (input.size == 2) {
-                    app.createCoin(input[1].toLong())
-                } else {
-                    app.createCoin()
-                }
+    while (true) {
+        val input = readLine()!!
 
-                logger.info("Generated Eurotoken. Your balance is now ${app.getBalance()} Eurotoken.")
-            }
-            "send_coin" -> {
-                if (input.size == 3) {
-                    app.sendCoin(input[1], input[2].toLong())
-                } else {
-                    app.sendCoin()
-                }
-
-                logger.info("Your balance is now ${app.getBalance()} Eurotoken.")
-            }
-            "transaction_history" -> {
-                if (input.size == 2) {
-                    app.transactionHistory(input[1])
-                }
-            }
-            "stop" -> {
-                app.stop()
-                running = false
-
-                logger.info("Shutting down...")
-            }
-            else -> {
-                logger.info("Unknown command")
-            }
+        if (!executeLine(input, app, logger)) {
+            break
         }
-
     }
 }
